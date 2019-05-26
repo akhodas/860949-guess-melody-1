1ff4d677137a69eeb6d5833b985f65d418467040
akhodas <a.l.khodas@gmail.com>
2019-05-26 20:48:14 +0300

    add reducer and test for its, add store and provider in index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app.jsx';
 import settings from './mocks/setting';
 import questions from './mocks/questions';
+import {reducer} from './reducer';
 
 const init = (gameSettings, gameQuestions) => {
+  const store = createStore(reducer);
 
   ReactDOM.render(
-      <App
-        {...gameSettings}
-        questions={gameQuestions}
-      />,
+      <Provider store={store}>
+        <App
+          {...gameSettings}
+          questions={gameQuestions}
+        />
+      </Provider>,
       document.querySelector(`.main`)
   );
 };
