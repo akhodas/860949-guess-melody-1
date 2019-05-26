7364e192d9354e18ca5ea8d601fc1f415179f717
akhodas <a.l.khodas@gmail.com>
2019-05-26 23:39:02 +0300

    connect storage to App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,42 +1,46 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from "react-redux";
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
+import {ActionCreator} from "../../reducer";
+
+
+const Type = {
+  ARTIST: `game--artist`,
+  GENRE: `game--genre`,
+};
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      question: -1,
-    };
-  }
-
-  _getScreen(question, onClick) {
+  _getScreen(question) {
     if (!question) {
       const {
         errorCount,
         gameTime,
+        onWelcomeScreenClick,
       } = this.props;
 
       return <WelcomeScreen
         errorCount={errorCount}
         gameTime={gameTime}
-        onClick={onClick}
+        onClick={onWelcomeScreenClick}
       />;
     }
+
+    const {onUserAnswer} = this.props;
 
     switch (question.type) {
       case `genre`: return <GenreQuestionScreen
         question={question}
-        onAnswer={onClick}
+        onAnswer={(userAnswer) => onUserAnswer(userAnswer, question)}
       />;
 
       case `artist`: return <ArtistQuestionScreen
         question={question}
-        onAnswer={onClick}
+        onAnswer={(userAnswer) => onUserAnswer(userAnswer, question)}
       />;
     }
 
@@ -44,21 +48,40 @@ class App extends Component {
   }
 
   render() {
-    const {questions} = this.props;
-    const {question} = this.state;
+    const {
+      questions,
+      step,
+    } = this.props;
 
-    return (this._getScreen(questions[question], (answer) => {
-      if (Array.isArray(answer)) {
-        // eslint-disable-next-line no-console
-        console.log(answer);
-      }
+    return <section className={`game ${Type.ARTIST}`}>
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{
+              filter: `url(#blur)`,
+              transform: `rotate(-90deg) scaleY(-1)`,
+              transformOrigin: `center`
+            }}
+          />
+        </svg>
+        <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+          <span className="timer__mins">05</span>
+          <span className="timer__dots">:</span>
+          <span className="timer__secs">00</span>
+        </div>
+        <div className="game__mistakes">
+          <div className="wrong"/>
+          <div className="wrong"/>
+          <div className="wrong"/>
+        </div>
+      </header>
 
-      this.setState({
-        question: question + 1 >= questions.length
-          ? -1
-          : question + 1,
-      });
-    }));
+      {this._getScreen(questions[step])}
+    </section>;
   }
 }
 
@@ -66,6 +89,26 @@ App.propTypes = {
   gameTime: PropTypes.number.isRequired,
   errorCount: PropTypes.number.isRequired,
   questions: PropTypes.array.isRequired,
+  step: PropTypes.number.isRequired,
+  onUserAnswer: PropTypes.func.isRequired,
+  onWelcomeScreenClick: PropTypes.func.isRequired,
 };
 
-export default App;
+const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
+  step: state.step,
+  mistakes: state.mistakes,
+});
+
+
+const mapDispatchToProps = (dispatch) => ({
+  onWelcomeScreenClick: () => dispatch(ActionCreator.incrementStep()),
+
+  onUserAnswer: (userAnswer, question) => {
+    dispatch(ActionCreator.incrementStep());
+    dispatch(ActionCreator.incrementMistake(userAnswer, question));
+  }
+});
+
+export {App};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
